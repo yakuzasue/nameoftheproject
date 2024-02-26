5af5155adf294b2973bfc50f588900fefce3c895
yakuzasue <111576861+yakuzasue@users.noreply.github.com>
2024-02-26 10:52:47 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,3 +2,11 @@
 module.exports = {
   reactStrictMode: true,
 }
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+}
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+}
